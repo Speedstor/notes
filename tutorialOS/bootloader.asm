19a557a9186d46b851ec0fad6e068c3ecb0938d0
Speedstor razerStealth <aldrin.cheung@gmail.com>
2021-12-19 02:50:20 -0800

    working bootloader with text printing

--- tutorialOS/bootloader.asm
+++ tutorialOS/bootloader.asm
@@ -5,13 +5,36 @@ bits 16
 start: jmp boot
 
 ;; constant and variable definitions
-msg db "Welcome to my Operating System!", 0ah, 0dh, 0ah
+msg db "Welcome to my Operating System!", 0ah, 0dh, 0ah, 0
 
 boot:
     cli ;no interrupts
     cld ; all that we need to init
+
+    ;mov al, 65
+    ;call PrintCharacter
+    mov si, msg
+    call PrintString
     hlt ; hlt the system
 
+PrintCharacter:
+    mov ah, 0x0E
+    mov bh, 0x00
+    mov bl, 0x07
+    int 0x10
+    ret
+
+PrintString:
+next_character:
+    mov al, [SI]
+    inc si
+    or al, al
+    jz exit_function
+    call PrintCharacter
+    jmp next_character
+exit_function:
+    ret 
+    ;jmp $
 ;; We have to be 512 bytes. Clear the rest of the bytes with 0
 times 510 - ($-$$) db 0
 dw 0xAA55
